918ad79fb80ddcbceeb823623bb90f88cde6d629
RBueno <renatobuenoamoros@gmail.com>
2020-11-09 17:36:59 +0100

    Clear code

---
+++ average.cs
@@ -0,0 +1,33 @@
+/*This program calculates marks' average, this marks are set by user entering
+ * them till user types end.
+*/
+
+using System;
+
+public class Average
+{
+    public static void Main()
+    {
+        double sumOfMarks = 0, individualMark, count = 0, average;
+        string mark;
+        
+        Console.Write("Insert a mark: ");
+        mark = Console.ReadLine(); 
+        
+        while(mark != "end")
+        {  
+            individualMark = Convert.ToDouble(mark);
+            count++;
+            sumOfMarks += individualMark;
+            
+            Console.Write("Insert a mark: ");
+            mark = Console.ReadLine();
+        }
+        
+        if(count != 0)
+        {
+            average = sumOfMarks / count;  
+            Console.WriteLine("The average is {0}.", average);
+        }
+    }
+}
